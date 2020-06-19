659d1def3995a10dbd6f53f3a8d84d7913ed4091
MASTER <diegohdz@ciencias.unam.mx>
2020-06-19 02:17:53 -0500

    Se añade un carrusel de fotos a la pantalla de Catalogo en tarea 3

--- Tarea3/app/src/main/java/com/example/tarea3/Catalogo.java
+++ Tarea3/app/src/main/java/com/example/tarea3/Catalogo.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Catalogo extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import ahmed.easyslider.EasySlider;
+import ahmed.easyslider.SliderItem;
+
+public class Catalogo extends AppCompatActivity {
+    EasySlider es;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo);
+        es = (EasySlider)findViewById(R.id.slider);
+        List<SliderItem> lista = new ArrayList<>();
+
+        lista.add(new SliderItem("Dental",R.drawable.doctor));
+        lista.add(new SliderItem("Doctor",R.drawable.dental));
+        lista.add(new SliderItem("Hand",R.drawable.mano));
+        lista.add(new SliderItem("Hospital",R.drawable.hospital));
+
+        es.setPages(lista);
     }
 }
